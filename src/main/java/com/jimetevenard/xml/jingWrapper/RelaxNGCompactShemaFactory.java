f03e28668afe76a4cce87ebde89b9cb3bf7ccb80
Jim Etevenard <jimetevenard@gmail.com>
2018-07-19 14:30:34 +0000

    first commit

---
+++ src/main/java/com/jimetevenard/xml/jingWrapper/RelaxNGCompactShemaFactory.java
@@ -0,0 +1,6 @@
+package com.jimetevenard.xml.jingWrapper;
+
+import com.thaiopensource.relaxng.jaxp.CompactSyntaxSchemaFactory;
+
+public class RelaxNGCompactShemaFactory extends CompactSyntaxSchemaFactory{
+}
---
+++ src/main/java/com/jimetevenard/xml/jingWrapper/RelaxNGSchemaFactory.java
@@ -0,0 +1,8 @@
+package com.jimetevenard.xml.jingWrapper;
+
+
+import com.thaiopensource.relaxng.jaxp.XMLSyntaxSchemaFactory;
+
+
+public class RelaxNGSchemaFactory extends XMLSyntaxSchemaFactory{
+}
